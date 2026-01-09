75dc927199bf2cae0a25881e773ecc013b2b95b7
EllaGliksberg <ellaglik@gmail.com>
2026-01-09 15:42:20 +0200

    get comments

--- src/routes/comment_routes.js
+++ src/routes/comment_routes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const commentController = require('../controllers/comment_controller');
 
 router.post('/', commentController.addComment);
+router.get('/', commentController.getAllComments);
+router.get('/:id', commentController.getCommentById);
+router.get('/post/:postId', commentController.getCommentsByPostId);
 
 module.exports = router;
